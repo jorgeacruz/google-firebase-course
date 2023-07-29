b5ecfc18899af58e05cc287ccec04441b3fd1f7a
Jorge.acruz <jorge.cruz@crmservives.com.br>
2023-07-29 13:26:06 -0300

    ToDo List - Files

---
+++ TodoList/js/auth.js
@@ -0,0 +1,28 @@
+authForm.onsubmit = function (event) {
+    event.preventDefault()
+    if (authForm.submitAuthForm.innerHTML == 'Acessar') {
+      firebase.auth().signInWithEmailAndPassword(authForm.email.value, authForm.password.value)
+      .catch(function (error) {
+        console.log('Falha no acesso')
+        console.log(error)
+      })
+    } else {
+      firebase.auth().createUserWithEmailAndPassword(authForm.email.value, authForm.password.value)
+      .catch(function (error) {
+        console.log('Falha no cadastro')
+        alert('Erro ao Cadastrar')
+      })
+  
+    }
+  }
+
+  firebase.auth().onAuthStateChanged(
+    function(user){
+      if(user){
+        console.log('Usuário autenticado')
+        console.log(user)
+      } else {
+        console.log('Usuário não autenticado')
+      }
+    }
+  )
---
+++ TodoList/js/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB-dRTWGQlWez1r9xTkq9oeJZuRD6iOH5w",
+  authDomain: "todolist-f7c87.firebaseapp.com",
+  projectId: "todolist-f7c87",
+  storageBucket: "todolist-f7c87.appspot.com",
+  messagingSenderId: "791301940291",
+  appId: "1:791301940291:web:214ea031b157a40870a0f6",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
